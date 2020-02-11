d6eb82b9adb37bd9ee787c12054a065e888cc70c
karolina-michalak <karolina0michalak@gmail.com>
2020-02-11 15:24:16 +0100

    added state for UserItem and added photo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/layouts/Navbar';
-import UserItem from '/components/users/UserItem.js';
+import UserItem from './components/users/UserItem.js';
 import './App.css';
 
 class App extends Component {
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 
 class UserItem extends Component {
+  state = {
+    id: 'id',
+    login: 'mojombo',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+    html_url: 'https://api.github.com/users/mojombo'
+  };
   render() {
-    return <div>UserItem</div>;
+    return (
+      <div className='card text-center'>
+        <img
+          src={this.state.avatar_url}
+          className='round-img'
+          style={{ width: '60px' }}
+        ></img>
+      </div>
+    );
   }
 }
 

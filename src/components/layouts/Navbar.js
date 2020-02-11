c98f17e44ef15fafa43a834e36327920a76c7f69
karolina-michalak <karolina0michalak@gmail.com>
2020-02-11 15:57:52 +0100

    destructured Navbar

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Navbar = props => {
+const Navbar = ({ icon, title }) => {
   return (
     <nav className='bg-primary navbar'>
       <h1>
-        <i className={props.icon} /> {props.title}
+        <i className={icon} /> {title}
       </h1>
     </nav>
   );

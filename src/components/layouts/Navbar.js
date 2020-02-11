b08a6bd5c2610b08487bf3db1094242f342db52d
karolina-michalak <karolina0michalak@gmail.com>
2020-02-11 15:11:54 +0100

    added PropTypes

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Navbar extends Component {
   static defaultProps = {
     title: 'GitHub Finder',
     icon: 'fab fa-github'
   };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  };
+
   render() {
     return (
       <nav className='bg-primary navbar'>

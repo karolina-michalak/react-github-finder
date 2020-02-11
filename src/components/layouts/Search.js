c53d3ff3d23b1d9b876dce1027b9b689771bd068
karolina-michalak <karolina0michalak@gmail.com>
2020-02-11 18:29:39 +0100

    search done and added prop types

--- src/components/layouts/Search.js
+++ src/components/layouts/Search.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
   state = {
     text: ''
   };
 
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired
+  };
+
   onSubmit = e => {
     e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({
+      text: ''
+    });
   };
 
   onChange = e => {

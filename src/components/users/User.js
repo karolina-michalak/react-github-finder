0f648081485c98d589f3d9fd42282565ecfa6185
Karolina Michalak <karolina0michalak@gmail.com>
2020-02-12 10:36:17 +0100

    created user UI and layout

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -16,7 +16,7 @@ class User extends Component {
   render() {
     const {
       name,
-      avatar,
+      avatar_url,
       url,
       location,
       bio,
@@ -26,7 +26,10 @@ class User extends Component {
       followers,
       following,
       public_repos,
-      public_gists
+      public_gists,
+      username,
+      website,
+      company
     } = this.props.user;
 
     const { loading } = this.props;
@@ -37,7 +40,66 @@ class User extends Component {
         <Link to="/" className="btn btn-light">
           Back to Search
         </Link>
-        {name}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              className="round-img"
+              alt=""
+              style={{ width: "100px" }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className="btn btn-dark my-1">
+              Visit GitHub profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="card text-center">
+            <div className="badge badge-primary">
+                Followers: {followers}
+            </div>
+            <div className="badge badge-success">
+                Following: {following}
+            </div>
+            <div className="badge badge-danger">
+                Public Repos: {public_repos}
+            </div>
+            <div className="badge badge-dark">
+                Public Gists: {public_gists}
+            </div>
+        </div>
       </Fragment>
     );
   }

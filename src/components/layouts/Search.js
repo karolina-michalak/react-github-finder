c4141898df88e55acc8bccfbfa398e7340fbeeff
Karolina Michalak <karolina0michalak@gmail.com>
2020-02-12 18:55:12 +0100

    created reducer and actions

--- src/components/layouts/Search.js
+++ src/components/layouts/Search.js
@@ -1,21 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import PropTypes from "prop-types";
+import GithubContext from "../../context/github/githubContext";
 
-const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
-const [text, setText] = useState('')
+const Search = ({ showClear, clearUsers, setAlert }) => {
+  const githubContext = useContext(GithubContext);
+  const [text, setText] = useState("");
 
   const onSubmit = e => {
     e.preventDefault();
     if (text === "") {
       setAlert("Please enter something", "light");
     } else {
-      searchUsers(text);
-      setText('')
+      githubContext.searchUsers(text);
+      setText("");
     }
   };
 
   const onChange = e => {
-    setText(e.target.value)
+    setText(e.target.value);
   };
 
   return (
@@ -44,7 +46,6 @@ const [text, setText] = useState('')
 };
 
 Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
   clearUsers: PropTypes.func.isRequired,
   showClear: PropTypes.bool.isRequired,
   setAlert: PropTypes.func.isRequired
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -20,13 +20,29 @@ const GithubState = props => {
 
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
+  const searchUsers = async text => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}}`
+    );
+
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items
+    });
+  };
+
+  const setLoading = () => dispatch({ type: SET_LOADING });
+
   return (
     <GitHubContext.Provider
       value={{
         users: state.users,
         user: state.user,
         repos: state.repos,
-        loading: state.loading
+        loading: state.loading,
+        searchUsers
       }}
     >
       {props.children}

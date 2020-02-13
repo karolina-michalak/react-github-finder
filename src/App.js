370114e2ebab56e745d8f60cbecda68c5d8dfb8a
Karolina Michalak <karolina0michalak@gmail.com>
2020-02-13 14:59:08 +0100

    created NotFound component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layouts/Navbar";
 import User from "./components/users/User.js";
@@ -7,6 +7,7 @@ import Alert from "./components/layouts/Alert";
 import About from "./components/pages/About";
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
+import NotFound from './components/pages/NotFound'
 import "./App.css";
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
